b918130f8c61f6bdde6cce0ac6695d49c6dda4eb
juan267 <j.gomez267@gmail.com>
2016-12-06 17:37:19 -0500

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,38 +1,94 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router'
 
-
-export class App extends Component {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      guests: [],
+      errors: {
+        firstName: false,
+        lastName: false
+      }
+    }
+  }
+  validateForm(firstName, lastName) {
+    if (!firstName || !lastName) {
+      this.setState({
+        errors: {
+          firstName: !firstName,
+          lastName: !lastName
+        }
+      })
+      return false
+    }
+    return true
+  }
+  addGuest(event) {
+    event.preventDefault()
+    const firstName = event.target['first-name'].value
+    const lastName = event.target['last-name'].value
+    if (this.validateForm(firstName, lastName)) {
+      const newGuest = {
+        firstName: firstName,
+        lastName: lastName,
+      }
+      this.setState({
+        guests: [...this.state.guests, newGuest],
+        errors: {
+          firstName: false,
+          lastName: false
+        }
+      })
+      event.target.reset()
+    }
+  }
+  renderGuest(guest, index) {
+    return (
+      <tr key={index}>
+        <td>{guest.firstName}</td>
+        <td>{guest.lastName}</td>
+      </tr>
+    )
+  }
   render() {
     return (
-      <div>
-        <nav>
-          {/*Aca deben ir los links de navegacion*/}
-        </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form onSubmit={this.addGuest.bind(this)}>
+              <div className={`form-group ${this.state.errors.firstName ? 'has-error' : null}`}>
+                <label htmlFor="first-name">Nombre</label>
+                <input type="text" className="form-control" name="first-name" />
+              </div>
+
+              <div className={`form-group ${this.state.errors.lastName ? 'has-error' : null}`}>
+                <label htmlFor="last-name">Apellido</label>
+                <input type="text" className="form-control" name="last-name" />
+              </div>
+
+              <div className="action">
+                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+              </div>
+            </form>
+
+            <table className="table bordered-table table-striped">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.guests.map(this.renderGuest)}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-// No tienes que hacer nada por debajo de esta linea
-//==================================================
+export default App
 
 
-export const Page1 = (props) => {
-  return (
-    <h1>Pagina 1</h1>
-  )
-}
-
-export const Page2 = (props) => {
-  return (
-    <h1>Pagina 2</h1>
-  )
-}
-
-export const NotFound = (props) => {
-  return (
-    <h1>Pagina no encontrada</h1>
-  )
-}
--- src/App.test.js
+++ src/App.test.js
@@ -1,58 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, shallow, mount } from 'enzyme';
-import {Router, Route, hashHistory, IndexRedirect, createMemoryHistory} from 'react-router'
-import { Simulate } from 'react-addons-test-utils'
-import {App, Page1, Page2, NotFound} from './App';
-
-const { click } = Simulate
-
-
-const routes = (
-  <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <IndexRedirect to='/page1'/>
-      <Route path='/page1' component={Page1}/>
-      <Route path='/page2' component={Page2}/>
-      <Route path='*' component={NotFound}/>
-    </Route>
-  </Router>
-)
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-it('renders Pagina 1 on load', () => {
-  const wrapper = mount(routes)
-  expect(wrapper.find('h1').text()).toBe('Pagina 1')
+it('renders a form with 3 inputs', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('form').children().length).toBe(3)
 })
 
-it('changes to Page2 component when clicking on page2 button', () => {
-  const wrapper = mount(routes)
-  click(wrapper.find('a[href="#/page2"]').get(0), { button: 0 })
-  expect(wrapper.find('h1').text()).toBe('Pagina 2')
+it("renders an input with name='first-name'", () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find("input[name='first-name']").length).toBe(1)
 })
 
-it('changes to Page1 component when clicking on page1 button', () => {
-  const wrapper = mount(routes)
-  click(wrapper.find('a[href="#/page2"]').get(0), { button: 0 })
-  expect(wrapper.find('h1').text()).toBe('Pagina 2')
-  click(wrapper.find('a[href="#/page1"]').get(0), { button: 0 })
-  expect(wrapper.find('h1').text()).toBe('Pagina 1')
+it("renders an input with name='last-name'", () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find("input[name='last-name']").length).toBe(1)
 })
 
-it('shows a not found page on routes that are not handle', () => {
-  const wrapper = mount(
-      <Router history={createMemoryHistory('/NoExiste')}>
-      <Route path='/' component={App}>
-        <IndexRedirect to='/page1'/>
-        <Route path='/page1' component={Page1}/>
-        <Route path='/page2' component={Page2}/>
-        <Route path='*' component={NotFound}/>
-      </Route>
-    </Router>
-  )
-  expect(wrapper.find('h1').text()).toBe('Pagina no encontrada')
+it('renders a table with two th one for Nombre and the other for Apellido', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find("table tr th").lenght).toBe(2)
 })
