fb042739872e181358a547de82a1844e206f8080
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-15 22:44:06 -0500

    app ok

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.change1 = this.change1.bind(this)
+    this.change2 = this.change2.bind(this)
+    this.addInvited = this.addInvited.bind(this)
+    this.state = {
+      list: [],
+      name:"",
+      lastname:""
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addInvited}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" value={this.state.name} onChange={this.change1} className="form-control" name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" value={this.state.lastname} onChange={this.change2} className="form-control" name="last-name" />
               </div>
 
               <div className="action">
@@ -30,7 +41,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.list.map((i) =>
+                  <tr>
+                    <td>{i.name}</td>
+                    <td>{i.lastname}</td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
@@ -38,6 +54,28 @@ class App extends Component {
       </div>
     )
   }
+
+  change1(event){
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  change2(event){
+    this.setState({
+      lastname: event.target.value
+    })
+  }
+
+  addInvited(event){
+    event.preventDefault();
+    this.setState({
+      list: this.state.list.concat({name: this.state.name, lastname: this.state.lastname}),
+      name: "",
+      lastname: ""
+    });
+  }
+
 }
 
 export default App
